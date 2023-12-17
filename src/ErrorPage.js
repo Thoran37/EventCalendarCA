c85d230597c61d126921e63546a541cd55eb2205
Thoran37 <132071612+Thoran37@users.noreply.github.com>
2023-12-17 18:13:15 +0530

    Update ErrorPage.js

--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function ErrorPage() {
+  let navigate = useNavigate()
+  function Home() {
+    navigate('')
+  }
   return (
-    <div></div>
+    <div className='text-danger display-1'>
+      Page Not Found
+      Please check the path of the page
+      <button className='btn btn-primary' onClick={Home}>Go to Main page</button>
+    </div>
   )
 }
 
