e07ac1ba9e887d0d5a1509afd6a7bda69c1689d1
Thoran37 <132071612+Thoran37@users.noreply.github.com>
2023-12-17 18:13:35 +0530

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from "react"
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='p-2 bg-dark'>
+      <h1 className='m-2 text-decoration-underline' style={{color:"#FF5B00"}}>Credits</h1>
+      <a href="#" className='link-offset-2-hover link-underline-hover link-light m-2'>M Thoran Chandra</a>
+      <a href='#' className='link-offset-2-hover link-underline-hover link-light m-2'>L Tejasree</a>
+      <br /><a href='#' className='link-offset-2-hover link-underline-hover link-light m-2'>M Sudeepthi</a>
+      <a href='#' className='link-offset-2-hover link-underline-hover link-light m-2'>Y BhanuTeja</a>
+    </div>
   )
 }
 
